07fe338843817c3618c0b1bd78b4228e52b7dd47
ramesh <rameshkumar.v@celestialsys.com>
2025-12-29 14:03:23 +0530

    added the sqlmodel and sql alchemy with postgresql and added lifespan using asynccontextmanager

--- src/__init__.py
+++ src/__init__.py
@@ -1,12 +1,23 @@
 from fastapi import FastAPI
 from src.books.routes import book_router
+from contextlib import asynccontextmanager
+
+
+@asynccontextmanager
+
+
+async def life_span(app:FastAPI):
+    print(f"Server is starting..................................")
+    yield
+    print(f"Server is ending....................................")
 
 version = "v1"
 
 app = FastAPI(
     title = "Sprintly",
     description = "Rest api for the books",
-    version= version
+    version= version,
+    lifespan=life_span
 )
 
 app.include_router(book_router, prefix=f"/api/{version}/books", tags=['books'])
